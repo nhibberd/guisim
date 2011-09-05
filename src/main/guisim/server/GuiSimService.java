afe109b37cf3ef85f6f7feedd3a6c5edd239ac5b
Nick Hibberd <nhibberd@gmail.com>
2011-09-05 15:22:06 +1000

    Output Stream

--- src/main/guisim/server/GuiSimService.java
+++ src/main/guisim/server/GuiSimService.java
@@ -4,18 +4,12 @@ import guisim.json.Flight;
 import guisim.load.HardwareEvents;
 import guisim.model.FromHardware;
 import guisim.model.Parse;
-import guisim.usb.Output;
-import scalaz.EnumerationW;
-import sun.security.util.Cache;
 
-import javax.xml.crypto.Data;
+import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class GuiSimService {
     private final HardwareEvents events = new HardwareEvents();
-    private Output outputWorker;
 
     public Flight poll() {
         FromHardware next = events.next();
@@ -26,12 +20,12 @@ public class GuiSimService {
         return flight;
     }
 
-    public void send(Flight data, OutputStream device) {
+    public void send(Flight data, OutputStream device) throws IOException {
         Parse parse = new Parse();
         byte[] outputData = new byte[6];
         System.arraycopy(parse.parseToBytes((short) data.roll),0,outputData,0,2);
         System.arraycopy(parse.parseToBytes((short) data.pitch),0,outputData,2,2);
-        System.arraycopy(parse.parseToBytes((short) data.yaw),0,outputData,4,2);
-        outputWorker.main(outputData, device);
+        System.arraycopy(parse.parseToBytes((short) data.yaw), 0, outputData, 4, 2);
+        device.write(outputData);
     }
 }
