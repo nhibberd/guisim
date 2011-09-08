9a9d00355d142671da30387a8c2978c296215590
unknown <nhibberd@gmail.com>
2011-09-08 12:08:50 +1000

    Serial Communication

--- src/main/guisim/server/GuiSim.java
+++ src/main/guisim/server/GuiSim.java
@@ -1,7 +1,8 @@
 package guisim.server;
 
 
-import guisim.load.HardwareLoader;
+
+import com.sun.org.apache.bcel.internal.util.ClassPath;
 import guisim.usb.Usb;
 import io.mth.foil.j.Foil;
 import io.mth.foil.j.Config;
@@ -10,15 +11,42 @@ import io.mth.foil.j.DefaultConfigs;
 import io.mth.foil.j.DefaultFoils;
 import io.mth.foil.j.Foils;
 
-import javax.comm.NoSuchPortException;
+
+import gnu.io.*;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.HashSet;
 
 public class GuiSim {
     private static final Foils foils = new DefaultFoils();
     private static final Configs c = new DefaultConfigs();
 
+    static Enumeration	      portList;
+    static CommPortIdentifier portId;
+    static String	      messageString = "Hello, world!";
+    static SerialPort	      serialPort;
+    static OutputStream       outputStream;
+    static boolean	      outputBufferEmptyFlag = false;
+
     public static void main(String[] args) {
+        if (args.length > 0) {
+            if(args[0].equals("COM3")){
+                SimpleWrite(args);
+            }
+        }
+
+        try
+		{
+			CommDriver RXTXDriver = (CommDriver) Class.forName("gnu.io.RXTXCommDriver").newInstance();
+			RXTXDriver.initialize();
+	        System.out.println("try");
+		}
+		catch (Throwable e)
+		{
+			System.err.println(e + " thrown while loading " + "gnu.io.RXTXCommDriver");
+		}
+
 
         Usb usb = new Usb();
         OutputStream device = null;
@@ -38,5 +66,80 @@ public class GuiSim {
                 ;
         Foil foil = foils.nu("guisim", 10080, config);
         foil.run();
+
+
+    }
+
+public static void SimpleWrite(String[] args) {
+    System.out.println("SimpleWrite");
+	boolean portFound = false;
+	String  defaultPort = "COM3";
+
+	if (args.length > 0) {
+	    defaultPort = args[0];
+	}
+
+	portList = CommPortIdentifier.getPortIdentifiers();
+
+	while (portList.hasMoreElements()) {
+	    portId = (CommPortIdentifier) portList.nextElement();
+
+	    if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+
+		if (portId.getName().equals(defaultPort)) {
+		    System.out.println("Found port " + defaultPort);
+
+		    portFound = true;
+
+		    try {
+			serialPort =
+			    (SerialPort) portId.open("SimpleWrite", 2000);
+		    } catch (PortInUseException e) {
+			System.out.println("Port in use.");
+
+			continue;
+		    }
+
+		    try {
+			outputStream = serialPort.getOutputStream();
+		    } catch (IOException e) {}
+
+		    try {
+			serialPort.setSerialPortParams(9600,
+						       SerialPort.DATABITS_8,
+						       SerialPort.STOPBITS_1,
+						       SerialPort.PARITY_NONE);
+		    } catch (UnsupportedCommOperationException e) {}
+
+
+		    try {
+		    	serialPort.notifyOnOutputEmpty(true);
+		    } catch (Exception e) {
+			System.out.println("Error setting event notification");
+			System.out.println(e.toString());
+			System.exit(-1);
+		    }
+
+
+		    System.out.println(
+		    	"Writing \""+messageString+"\" to "
+			+serialPort.getName());
+
+		    try {
+			outputStream.write(messageString.getBytes());
+		    } catch (IOException e) {}
+
+		    try {
+		       Thread.sleep(2000);  // Be sure data is xferred before closing
+		    } catch (Exception e) {}
+		    serialPort.close();
+		    System.exit(1);
+		}
+	    }
+	}
+
+	if (!portFound) {
+	    System.out.println("port " + defaultPort + " not found.");
+	}
     }
 }
--- src/main/guisim/usb/Usb.java
+++ src/main/guisim/usb/Usb.java
@@ -1,8 +1,7 @@
 package guisim.usb;
 
+import gnu.io.*;
 import guisim.load.HardwareLoader;
-
-import javax.comm.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,13 +9,44 @@ import java.io.OutputStream;
 public class Usb {
 
     public OutputStream start() throws NoSuchPortException, IOException {
-        CommPortIdentifier communicationPort = CommPortIdentifier.getPortIdentifier("asd");  //arduino ID/name?
+        try
+		{
+			CommDriver RXTXDriver = (CommDriver) Class.forName("gnu.io.RXTXCommDriver").newInstance();
+			RXTXDriver.initialize();
+		}
+		catch (Throwable e)
+		{
+			System.err.println(e + " thrown while loading " + "gnu.io.RXTXCommDriver");
+		}
+        /*
+        String driverName = "com.sun.comm.Win32Driver";
+        try{
+            CommDriver commdriver =
+                    (CommDriver)Class.forName(driverName).newInstance();
+            commdriver.initialize();
+        }
+        catch (Exception e2){
+            e2.printStackTrace();
+        }*/
 
+
+        CommPortIdentifier communicationPort = null;
+        String thisPort = "COM3";
+        try{
+            communicationPort = CommPortIdentifier.getPortIdentifier(thisPort);  //arduino ID/name?
+        } catch(NoSuchPortException e) {
+            //error
+            System.err.println("No such port " + thisPort);
+        }
         SerialPort port = null;
         try{
-            port = (SerialPort) communicationPort.open("arduion", 10);
+            if (communicationPort != null) {
+                port = (SerialPort) communicationPort.open("Arduion UNO", 10);
+                System.out.println(".open");
+            }
         } catch(PortInUseException e) {
             //error
+            System.err.println(thisPort + " is in use.");
         }
         try {
             if (port != null) {
